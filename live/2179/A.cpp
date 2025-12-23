d768befc7d1e80dfc81ac216a5053173338f8a63
Ahmad Saeed Zaidi <ahmadsaeedzaidi@gmail.com>
2025-12-23 22:09:41 +0500

    i did contest 2179

--- live/2179/A.cpp
+++ live/2179/A.cpp
@@ -7,14 +7,15 @@ typedef pair<int, int> pi;
 #define S second
 #define PB push_back
 #define MP make_pair
-#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REP(i, a, b) for (int i = a; i <= b; i++)
 #define SQ(a) (a) * (a)
 
 int main() {
   int t;
   cin >> t;
   while (t--) {
-    int n;
-    cin >> n;
+    int k,x;
+    cin >> k >> x;
+    cout << x*k + 1 << "\n";
   }
 }
---
+++ live/2179/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SQ(a) (a) * (a)
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, sum = 0, curr = 0, min_sum = INT_MAX;
+    cin >> n;
+    int a[n];
+    REP(i, 0, n) {
+      cin >> a[i];
+      if (i>0) sum += abs(a[i]-a[i-1]);
+    }
+    REP(i, 0, n) {
+      if (i == 0) {
+        curr = sum- abs(a[0] - a[1]);
+      } else if (i == n - 1) {
+        curr = sum- abs(a[n-1] - a[n-2]);
+      } else {
+        curr = sum- abs(a[i] - a[i + 1]) - abs(a[i] - a[i - 1]) + abs(a[i - 1] - a[i + 1]);
+      }
+      min_sum = min(min_sum,curr);
+    }
+    cout << min_sum << "\n";
+  }
+}
