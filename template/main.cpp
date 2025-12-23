3c35bcb1b879a0697d3b8b04f92efefbc2de0c0c
Ahmad Saeed Zaidi <ahmadsaeedzaidi@gmail.com>
2025-12-23 13:41:24 +0500

    added small addition to template

--- template/main.cpp
+++ template/main.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
 #define F first
 #define S second
 #define PB push_back
 #define MP make_pair
-#define REP(i,a,b) for (int i = a; i <= b; i++)
-#define SQ(a) (a)*(a)
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define SQ(a) (a) * (a)
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+  }
+}
