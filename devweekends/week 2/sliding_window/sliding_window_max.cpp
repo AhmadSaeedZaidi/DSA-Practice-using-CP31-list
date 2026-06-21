905181974d34737840d96c7e292dfaef613cd9da
AhmadSaeedZaidi <ahmadsaeedzaidi@gmail.com>
2026-06-21 23:50:43 +0500

    even better solution

--- devweekends/week 2/sliding_window/sliding_window_max.cpp
+++ devweekends/week 2/sliding_window/sliding_window_max.cpp
@@ -5,8 +5,8 @@ typedef vector<int> vi;
 typedef pair<int, int> pi;
 typedef unordered_map<int, bool> mi;
 typedef unordered_set<int> si;
-typedef priority_queue<pi> pq;
-typedef deque<pi> dq;
+typedef priority_queue<int_least32_t> pq;
+typedef deque<int> dq;
 #define F first
 #define S second
 #define PB push_back
@@ -19,19 +19,19 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         dq q;
         vi sol;
-        int i=0,j, n = nums.size();
-        q.push_front({nums[0], 0});
-        for (j = 1; j<k;j++) {
-            while (!q.empty() && q.back().F < nums[j]) q.pop_back();
-            q.push_back({nums[j],j});
-        }
-        sol.push_back(q.front().F);
-        for (j=k;j < n; j++) {
-            i++;
-            while (!q.empty() && (q.front().S < i)) q.pop_front();
-            while (!q.empty() && q.back().F < nums[j]) q.pop_back();
-            q.push_back({nums[j],j});
-            sol.push_back(q.front().F);
+        int i=0;
+        // q.push_front({nums[0], 0});
+        // for (j = 1; j<k;j++) {
+        //     while (!q.empty() && q.back().F < nums[j]) q.pop_back();
+        //     q.push_back({nums[j],j});
+        // }
+        // sol.push_back(q.front().F);
+        for (i=0;i < nums.size(); i++) {
+            while (!q.empty() && q.front() < i-k) q.pop_front();
+            while (!q.empty() && nums[q.back()] < nums[i]) q.pop_back();
+            q.push_back(i);
+
+            if (i > k) sol.push_back(nums[q.front()]);
            
         }
         return sol;
